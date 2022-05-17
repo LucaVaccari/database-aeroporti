907efe6c7d9fd7b401306d8c0b93f4a9a67452b1
Luca Vaccari <lucavaccari03@gmail.com>
2022-05-17 09:48:14 +0200

    create html table

--- parte_significativa.php
+++ parte_significativa.php
@@ -1,1 +1,23 @@
-<?php
+ <table>
+     <tr>
+         <th>id addetto</th>
+         <th>punto controllo</th>
+         <th>inizio controllo</th>
+         <th>durata controllo</th>
+         <th>note</th>
+     </tr>
+     <?php
+        $connection = mysqli_connect("localhost", "root", "", "aeroportomln");
+        $query = "SELECT * FROM controlli";
+        $controlli = mysqli_query($connection, $query);
+        echo "<tr>";
+        while ($row = mysqli_fetch_array($controlli)) {
+            echo "<td>$row[id_addetto]</td>";
+            echo "<td>$row[punto_controllo]</td>";
+            echo "<td>$row[inizio_controllo]</td>";
+            echo "<td>$row[durata_controllo]</td>";
+            echo "<td>$row[note]</td>";
+        }
+        echo "</tr>";
+        ?>
+ </table>
